9d85045e4c53888962a59d18f974f816a7d33b3b
SivaSahitya <82566266+SivaSahitya@users.noreply.github.com>
2021-04-15 15:02:34 +0530

    Create ex.py

---
+++ ex.py
@@ -0,0 +1,21 @@
+>>> #declare a variable
+>>> a=10
+>>> print(a)
+10
+>>> b=20.3
+>>> print(b)
+20.3
+>>> #to get the datatype
+>>> print(type(b))
+<class 'float'>
+>>> dt=type(a)
+>>> print(dt)
+<class 'int'>
+>>> c='a'
+>>> print(c)
+a
+>>> print(type(c))
+<class 'str'>
+>>> #print string along with variable value
+>>> print('a is ', a)
+a is  10
