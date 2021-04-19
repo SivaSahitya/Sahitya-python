7cf4a406662ec9cfe7438ab59a6c845eae0e91f6
SivaSahitya <82566266+SivaSahitya@users.noreply.github.com>
2021-04-19 11:33:40 +0530

    Create Operators.py

---
+++ Operators.py
@@ -0,0 +1,14 @@
+a=20
+b=3
+c=a+b
+print('sum is:',c)
+c=a-b
+print('sub is:',c)
+c=a*b
+print('mul is:',c)
+c=a/b
+print('div is:',c)
+c=a//b
+print('floor div is:',c)
+c=a**b
+print('exponent is:',c)
